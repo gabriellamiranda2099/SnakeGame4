ce3564d21a991ebc7dfb33bddee25604f25864f7
gabriellamiranda2099 <gabriella.miranda.leite@escola.pr.gov.br>
2026-09-21 11:51:32 +0000

    versao4

---
+++ board.java
@@ -0,0 +1,13 @@
+public class Board{
+    public void draw(Graphics2D g2) {
+        drawGrass (g2)
+    }
+
+
+    private void drawGrass (Graphics2D g2)  [
+        int tile = GameConstants, TILE;
+ 
+            
+    
+    ]
+}
